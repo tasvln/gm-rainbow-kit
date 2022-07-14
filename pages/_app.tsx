ed1018fe6c6d399cb7b2200090a65478953a32a3
Adebayo Temitope <adecancode@gmail.com>
2022-07-14 21:08:28 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,55 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+
+// rainbow kit & wagmi imports
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyId } from 'utils/variables';
+
+// rainbow kit & wagmi configs
+const { 
+  chains, 
+  provider
+} = configureChains(
+  [
+    chain.mainnet, 
+    chain.polygon, 
+    chain.optimism, 
+    chain.arbitrum
+  ],
+  [
+    alchemyProvider({ alchemyId: alchemyId }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
 
 export default MyApp
---
+++ utils/variables.ts
@@ -0,0 +1,1 @@
+export const alchemyId: string | undefined = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
